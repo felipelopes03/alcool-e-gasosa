c90093bfc45b3ae4270314230880e1226fadd951
felipelopes03 <165731375+felipelopes03@users.noreply.github.com>
2024-06-20 20:36:11 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿﻿Console.Clear();
+
+Console.WriteLine("--- Alcool ou Gasolina? ---");
+
+double alcool;
+double gasolina;
+
+Console.WriteLine("Digite o preço do alcool (R$):");
+alcool = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Digite o preço da gasolina (R$):");
+gasolina = Convert.ToDouble(Console.ReadLine());
+
+Calcular();
+
+void Calcular()
+{
+    double percentual = (alcool / gasolina) * 100;
+
+    Console.WriteLine("O preço do alcool corresponde a " + percentual.ToString("n2") + "% do preço da gasolina.");
+
+    Console.ForegroundColor = ConsoleColor.Green;
+    if (percentual > 70)
+    {
+        Console.WriteLine("Recomendação: Abasteça com gasolina.");
+    }
+    else 
+    {
+        Console.WriteLine("Recomendação: Abasteça com alcool.");
+    }
+    Console.ResetColor();
+}
